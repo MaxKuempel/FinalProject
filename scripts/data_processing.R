40e7263882308ef76d1a3652a0862a3cb7c9dea8
MaxKuempel <kuempema@oregonstate.edu>
2026-03-03 15:42:43 -0800

    Join in R

--- scripts/data_processing.R
+++ scripts/data_processing.R
@@ -2,6 +2,7 @@
 
 #lib
 library(readxl)
+library(tidyverse)
 
 #read import and exports
 e_2023 <- read_excel("data/Imports_Exports_2023.xlsx", sheet = "Exports")
@@ -9,3 +10,7 @@ i_2023 <- read_excel("data/Imports_Exports_2023.xlsx", sheet = "Imports")
 
 #read port coords
 portcord <- read_excel("data/portcoords.xlsx")
+portcord$`Schedule K Code` <- as.numeric(portcord$`Schedule K Code`)
+ 
+#join data
+test <- inner_join(e_2023, portcord, join_by("FORPORT" == "Schedule K Code"))
